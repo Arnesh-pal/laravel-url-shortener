4b67cd3fa88d2b6d199287f4a93fb53f64e112df
Arnesh Pal <arneshpal@Arneshs-MacBook-Air.local>
2025-08-09 12:48:52 +0530

    Fix: Configure Nginx to use dynamic port for Render deployment

--- docker/start.sh
+++ docker/start.sh
@@ -7,7 +7,9 @@ set -e
 php /var/www/html/artisan migrate --force
 
 # Replace the PORT variable in the Nginx config
-envsubst '$PORT' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.temp
+# Replace ${PORT} with actual port from Render
+envsubst '$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
 mv /etc/nginx/conf.d/default.conf.temp /etc/nginx/conf.d/default.conf
 
 # Start PHP-FPM in the background
