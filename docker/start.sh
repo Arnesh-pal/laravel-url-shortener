8b09732782bd9ec5a256694e7caaf4e4e9a5c2af
Arnesh Pal <arneshpal@Arneshs-MacBook-Air.local>
2025-08-10 09:35:24 +0530

    FINAL FIX: Simplify startup script to resolve APP_KEY issue

--- docker/start.sh
+++ docker/start.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e
 
-# Run database migrations
+# ONLY run database migrations. No more caching commands.
 php /var/www/html/artisan migrate --force
-
-# Substitute the PORT variable into the Nginx config template
-envsubst '$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Start PHP-FPM in the background
 php-fpm &
